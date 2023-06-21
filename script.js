296553a67141e4c7bd531773b7b69023ead1b6d2
soysauce4444 <105949254+soysauce4444@users.noreply.github.com>
2023-06-21 14:44:11 -0400

    Create getNum, getDivs, start getSqrt

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const container = document.querySelector('.container');
 
-for (i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
+function getNum () {
+    num = prompt("Enter a value for n: ", 1)
+    return num;
 }
+
+function getDivs() {
+    for (i = 0; i < 16; i++) {
+        const div = document.createElement('div');
+        container.appendChild(div);
+        //num++;
+    }
+    //console.log(num)
+    //return num;
+}
+
+function getSqrt(num) {
+    let i = 1;
+    while (i * i < num) {
+        i++;
+        (i * i == num) ? sqrt = i :
+        ++sqrt;
+    }
+    return (Math.floor(sqrt));
+}
+
+let num = 0;
+let sqrt = 1;
+getNum();
+getDivs();
+console.log(getSqrt(num));
