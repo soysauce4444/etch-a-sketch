79ff1f27ee51b02617ce389f88c809ae76a821ff
soysauce4444 <105949254+soysauce4444@users.noreply.github.com>
2023-06-23 15:14:09 -0400

    Center container, add button

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function makeDivs() {
     for (i = 0; i < 16; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
-        //num++;
     }
     const divs = document.querySelectorAll('.container > div');
     divs.forEach((div) => {
@@ -17,27 +16,7 @@ function makeDivs() {
             div.classList.add('changeClr');
         });
     });
-    //console.log(num)
-    //return num;
 }
-
-
-/*
-const divs = document.querySelectorAll('.container > div');
-divs.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.classList.add('changeClr');
-    });
-});
-*/
 
 let num = 0;
 makeDivs();
-
-/*
-const div = document.querySelector('div');
-
-div.addEventListener('mouseover', () => {
-    div.classList.add('changeClr');
-});
-*/
