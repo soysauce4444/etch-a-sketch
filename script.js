f1a6ea2abebf6b55102ca8dc5be28eacf59178e3
Youngin Choi <105949254+soysauce4444@users.noreply.github.com>
2023-06-26 18:28:00 -0400

    Clean styles, add setGrid, change getNum
    Remove flex property. Limit # of squares in each
    row in getNum.

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const container = document.querySelector('.container');
 
 let num = 0;
 function getNum () {
-    const x = prompt("Enter a value for n: ", 1);
-    return parseInt(x);
+    let x = 1;
+    while (x) {
+        const x = prompt("Enter a value for n: ", 4);
+        if (x <= 100) {
+            return parseInt(x);
+        }
+        else {
+            alert('Please enter a value below 100');
+            continue;
+        }
+    }
 }
 
 function makeDivs(y) {
@@ -22,13 +31,24 @@ function changeClr() {
     });
 }
 
-function setGrid() {
+function makeGrid() {
     const btn = document.querySelector('button');
     btn.addEventListener('click', () => {
-        num = getNum();
+        num = getNum() ** 2;
         makeDivs(num);
         changeClr();
+        setGrid();
     });
 }
 
-setGrid();
+let ratio = 0.0;
+function setGrid () {
+    const divs = document.querySelectorAll('.container > div');
+    const ratio = 1 / Math.sqrt(num) * 100;
+    console.log(ratio);
+    divs.forEach((div) => {
+        div.style.flex = `0 0 ${ratio}%`;
+    });
+}
+
+makeGrid();
